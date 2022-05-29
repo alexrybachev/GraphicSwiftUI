4376150492d13f67a8843b952c15a03250644251
Alex Rybachev <rybachev.as@gmail.com>
2022-05-29 22:12:49 +0400

    initial

---
+++ GraphicSwiftUI/HalfOfMuzzle.swift
@@ -0,0 +1,20 @@
+//
+//  HalfOfMuzzle.swift
+//  GraphicSwiftUI
+//
+//  Created by Aleksandr Rybachev on 29.05.2022.
+//
+
+import SwiftUI
+
+struct HalfOfMuzzle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HalfOfMuzzle_Previews: PreviewProvider {
+    static var previews: some View {
+        HalfOfMuzzle()
+    }
+}
