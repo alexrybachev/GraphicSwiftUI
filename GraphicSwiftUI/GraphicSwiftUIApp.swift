d5ac2a65e6ebfdda23080f057b2c11cec6efc5e5
Alex Rybachev <rybachev.as@gmail.com>
2022-05-29 22:11:09 +0400

    Initial Commit

---
+++ GraphicSwiftUI/GraphicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicSwiftUIApp.swift
+//  GraphicSwiftUI
+//
+//  Created by Aleksandr Rybachev on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
